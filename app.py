dcf6369f24690bd676a48d348f04a98d0ab09886
Sudeep Potluri <sspfzr@umsystem.edu>
2022-12-09 18:36:30 -0600

    Login, Register, Profile all work :: Addmovie works but need to be worked on :: Edit movie does not work

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def login():
             session['loggedin'] = True
             session['username'] = account['username']
             msg = 'Logged in successfully !'
-            return render_template('user.html', msg = msg)
+            return render_template('home.html', msg = msg)
         else:
             msg = 'Incorrect username / password !'
     return render_template('login.html', msg = msg)
@@ -70,8 +70,8 @@ def register():
         msg = 'Please fill out the form !'
     return render_template('register.html', msg = msg)
  
-@app.route('/movie', methods =['GET', 'POST'])
-def movie(): #This fuction will not be ready until the database is set up 
+@app.route('/addmovie', methods =['GET', 'POST'])
+def addmovie(): #Need to add in genre, platform, user and platform ratings  
     msg = ''
     if request.method == 'POST' and 'movieTitle' in request.form and 'genre' in request.form and 'platform' in request.form and 'summary' in request.form and 'movieTime' in request.form and 'director' in request.form and 'moviePoster' in request.form:
         movieTitle = request.form['movieTitle']
@@ -82,12 +82,37 @@ def movie(): #This fuction will not be ready until the database is set up
         director = request.form['director']
         moviePoster = request.form['moviePoster'] #Not in databse yet 
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('INSERT INTO movie (movieTitle,Summary,Director,movie_time,moviePoster) VALUES (% s, % s, % s, % s, % s)', (movieTitle, summary, director, movieTime, moviePoster,  )) #Fix this line as it will not insert right now 
+        cursor.execute('INSERT INTO movie (movieTitle,Summary,Director,movie_time,moviePoster) VALUES (% s, % s, % s, % s, % s)', (movieTitle, summary, director, movieTime, moviePoster,  )) 
         mysql.connection.commit()
         msg = 'You have successfully added your movie!'
     elif request.method == 'POST':
         msg = 'Please fill out the form !'
-    return render_template('movie.html', msg = msg)
+    return render_template('addmovie.html', msg = msg)
+
+@app.route('/profile', methods =['GET', 'POST'])
+def profile():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'fname' in request.form and 'mname' in request.form and 'lname' in request.form and 'language' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        fname = request.form['fname']
+        mname = request.form['mname'] 
+        lname = request.form['lname']
+        language = request.form['language']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE username = % s', (username, ))
+        user = cursor.fetchone()
+        if user:
+            msg = 'Account already exists !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'name must contain only characters and numbers !'
+        else:
+            cursor.execute('UPDATE user SET  username =% s, password =% s, fname =% s, mname =% s, lname =% s, language =% s WHERE username =% s', (username, password, fname, mname, lname, language, (session['username'], ), ))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('profile.html', msg = msg)
 
 if __name__ == "__main__":
     app.run(host ="localhost", port = int("5000"))
